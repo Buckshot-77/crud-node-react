fe2f804ddddb54f8c13fa7bc9d25f9bb948b3bad
Hugo Oliveira <hugo684@gmail.com>
2020-09-28 17:06:59 -0300

    removido cors das rotas

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const cors = require('cors');
 
 const productController = require('../controllers/productController');
 
 router.delete('/wipeAll', productController.wipeDB);
 router
   .route('/')
-  .get(cors(), productController.getAllProducts)
+  .get(productController.getAllProducts)
   .post(productController.postNewProduct);
 
 router

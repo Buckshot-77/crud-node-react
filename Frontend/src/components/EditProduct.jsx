27e17dea01d782bb128a8a432e94be1a68d1961a
Hugo Oliveira <hugo684@gmail.com>
2020-09-28 21:37:06 -0300

    Atualização em "EditProduct.jsx"

--- Frontend/src/components/EditProduct.jsx
+++ Frontend/src/components/EditProduct.jsx
@@ -1,21 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import axios from 'axios';
 
-export const EditProduct = () => {
+export const EditProduct = (props) => {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(null);
+  const [price, setPrice] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const fetchedProduct = await (
+        await axios.get(`http://localhost:3001/${props.match.params.id}`)
+      ).data.products;
+      setName(fetchedProduct.item);
+      setQuantity(fetchedProduct.quantidade);
+      setPrice(fetchedProduct.valor);
+    })();
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.patch(`http://localhost:3001/${props.match.params.id}`, {
+      item: name,
+      quantidade: quantity,
+      valor: price,
+    });
+    console.log(name, quantity, price)
+    // props.history.push('/');
+  };
+
   return (
-    <Form>
+    <Form onSubmit={(e) => handleSubmit(e)}>
       <FormGroup>
-        <Label>Name</Label>
-        <Input type="text" placeholder="Enter Name"></Input>
-        <Label>Quantity</Label>
-        <Input type="number" placeholder="Enter Quantity"></Input>
-        <Label>Price</Label>
-        <Input type="number" placeholder="Enter Price"></Input>
+        <Label>Nome</Label>
+        <Input
+          type="text"
+          placeholder="Enter Name"
+          defaultValue={name}
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+        <Label>Quantidade</Label>
+        <Input
+          type="number"
+          placeholder="Enter Quantity"
+          step="1"
+          min="0"
+          defaultValue={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        ></Input>
+        <Label>Preço</Label>
+        <Input
+          type="number"
+          placeholder="Enter Price"
+          defaultValue={price}
+          step=".01"
+          min="0"
+          onChange={(e) => setPrice(e.target.value)}
+        ></Input>
       </FormGroup>
-      <Button type="submit">Edit name</Button>
+      <Button type="submit">Editar Nome</Button>
       <Link to="/" className="btn btn-danger ml-2">
-        Cancel
+        Cancelar
       </Link>
     </Form>
   );
